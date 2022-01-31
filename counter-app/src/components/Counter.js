937099a8b66fa56d033965037c365cef969c647c
ayseilkay <aayseilkay@gmail.com>
2022-01-31 23:55:52 +0300

    ceated reducers and initialState

---
+++ counter-app/src/components/Counter.js
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+
+function Counter(){
+    const countValue = useSelector((state) => state.counter.value)
+    console.log(countValue);
+    return (<div> 
+        <h1>{countValue} </h1>
+    </div>)
+}
+
+export default Counter;
